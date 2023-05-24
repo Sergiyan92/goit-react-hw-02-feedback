8ac88b016a7d006ee158f56db6afead0485a255e
Sergiyan92 <sergiyan@ukr.net>
2023-05-24 18:28:52 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
+import { Statistics } from './Viget';
+import { FeedbackOptions } from './Viget';
+import { Section } from './Viget';
+import css from './Viget.module.css';
+import React, { useState } from 'react';
 export const App = () => {
+  <div
+    style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 40,
+      color: '#010101',
+    }}
+  />;
+
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = category => {
+    setFeedback(prevState => ({
+      ...prevState,
+      [category]: prevState[category] + 1,
+    }));
+  };
+
+  const { good, neutral, bad } = feedback;
+  const totalFeedback = good + neutral + bad;
+  const positivePercentage =
+    totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+
+  const options = Object.keys(feedback);
+  const hasFeedback = totalFeedback > 0;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.feedback}>
+      <Section>
+        <FeedbackOptions options={options} onLeaveFeedback={handleFeedback} />
+      </Section>
+
+      <Section>
+        {hasFeedback ? (
+          <Statistics
+            className={css.list}
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <p className={css.notification}>There is no feedback</p>
+        )}
+      </Section>
     </div>
   );
 };
---
+++ src/components/Viget.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import css from './Viget.module.css';
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <div className={css.statistics}>
+      <h2 className={css.title}>Statistics</h2>
+      <p className={css.subtitle}>Good: {good}</p>
+      <p className={css.subtitle}>Neutral: {neutral}</p>
+      <p className={css.subtitle}>Bad: {bad}</p>
+      <p className={css.subtitle}>Total: {total}</p>
+      <p className={css.subtitle}>Positive feedback: {positivePercentage}%</p>
+    </div>
+  );
+};
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      <h2 className={css.title}>Please leave feedback</h2>
+      {options.map(option => (
+        <button
+          className={css.button}
+          key={option}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export const Section = ({ title, children }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+};
