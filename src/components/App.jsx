2058dd6c08109d2497570a66ea6b00d3e9ee2067
Sergiyan92 <sergiyan@ukr.net>
2023-05-25 19:51:33 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,55 @@
+import React, { Component } from 'react';
 import { Statistics } from './Viget';
 import { FeedbackOptions } from './Viget';
 import { Section } from './Viget';
+import { Notification } from './Viget';
 import css from './Viget.module.css';
-import React, { useState } from 'react';
-export const App = () => {
-  <div
-    style={{
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: 40,
-      color: '#010101',
-    }}
-  />;
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
-
-  const handleFeedback = category => {
-    setFeedback(prevState => ({
-      ...prevState,
+  handleFeedback = category => {
+    this.setState(prevState => ({
       [category]: prevState[category] + 1,
     }));
   };
 
-  const { good, neutral, bad } = feedback;
-  const totalFeedback = good + neutral + bad;
-  const positivePercentage =
-    totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = good + neutral + bad;
+    const positivePercentage =
+      totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
 
-  const options = Object.keys(feedback);
-  const hasFeedback = totalFeedback > 0;
+    const options = Object.keys(this.state);
+    const hasFeedback = totalFeedback > 0;
 
-  return (
-    <div className={css.feedback}>
-      <Section>
-        <FeedbackOptions options={options} onLeaveFeedback={handleFeedback} />
-        {hasFeedback ? (
-          <Statistics
-            className={css.list}
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={totalFeedback}
-            positivePercentage={positivePercentage}
+    return (
+      <div className={css.feedback}>
+        <Section>
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleFeedback}
           />
-        ) : (
-          <p className={css.notification}>No feedback given</p>
-        )}
-      </Section>
-    </div>
-  );
-};
+        </Section>
+
+        <Section title="Statistics">
+          {hasFeedback ? (
+            <Statistics
+              className={css.list}
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
--- src/components/Viget.jsx
+++ src/components/Viget.jsx
@@ -1,45 +1,61 @@
 import css from './Viget.module.css';
-export const Statistics = ({
-  good,
-  neutral,
-  bad,
-  total,
-  positivePercentage,
-}) => {
-  return (
-    <div className={css.statistics}>
-      <h2 className={css.title}>Statistics</h2>
-      <p className={css.subtitle}>Good: {good}</p>
-      <p className={css.subtitle}>Neutral: {neutral}</p>
-      <p className={css.subtitle}>Bad: {bad}</p>
-      <p className={css.subtitle}>Total: {total}</p>
-      <p className={css.subtitle}>Positive feedback: {positivePercentage}%</p>
-    </div>
-  );
-};
+import React, { Component } from 'react';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  return (
-    <div>
-      <h2 className={css.title}>Please leave feedback</h2>
-      {options.map(option => (
-        <button
-          className={css.button}
-          key={option}
-          onClick={() => onLeaveFeedback(option)}
-        >
-          {option}
-        </button>
-      ))}
-    </div>
-  );
-};
+export class Statistics extends Component {
+  render() {
+    const { good, neutral, bad, total, positivePercentage } = this.props;
 
-export const Section = ({ title, children }) => {
-  return (
-    <div>
-      <h2>{title}</h2>
-      {children}
-    </div>
-  );
-};
+    return (
+      <div className={css.statistics}>
+        {/* <h2 className={css.title}>Статистика</h2> */}
+        <p className={css.subtitle}>Good: {good}</p>
+        <p className={css.subtitle}>Neutral: {neutral}</p>
+        <p className={css.subtitle}>Bad: {bad}</p>
+        <p className={css.subtitle}>Total: {total}</p>
+        <p className={css.subtitle}>Positive feedback: {positivePercentage}%</p>
+      </div>
+    );
+  }
+}
+
+export class FeedbackOptions extends Component {
+  render() {
+    const { options, onLeaveFeedback } = this.props;
+
+    return (
+      <div>
+        <h2 className={css.title}>Please leave feedback</h2>
+        {options.map(option => (
+          <button
+            className={css.button}
+            key={option}
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
+
+export class Section extends Component {
+  render() {
+    const { title, children } = this.props;
+
+    return (
+      <div>
+        <h2 className={css.title}>{title}</h2>
+        {children}
+      </div>
+    );
+  }
+}
+
+export class Notification extends Component {
+  render() {
+    const { message } = this.props;
+
+    return <p className={css.subtitle}>{message}</p>;
+  }
+}
