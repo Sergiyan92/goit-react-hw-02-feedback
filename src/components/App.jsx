4ddca87350a26ca0c68bc0ce7807fef59275c792
Sergiyan92 <sergiyan@ukr.net>
2023-05-26 20:26:14 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,21 +18,30 @@ export class App extends Component {
     }));
   };
 
+  TotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  PositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const totalFeedback = this.TotalFeedback();
+    return totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+  };
   render() {
     const { good, neutral, bad } = this.state;
-    const totalFeedback = good + neutral + bad;
-    const positivePercentage =
-      totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
-
     const options = Object.keys(this.state);
-    const hasFeedback = totalFeedback > 0;
+    const hasFeedback = this.TotalFeedback() > 0;
 
     return (
       <div className={css.feedback}>
-        <FeedbackOptions
-          options={options}
-          onLeaveFeedback={this.handleFeedback}
-        />
+        <Section title="">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+
         <Section title="Statistics">
           {hasFeedback ? (
             <Statistics
@@ -40,8 +49,8 @@ export class App extends Component {
               good={good}
               neutral={neutral}
               bad={bad}
-              total={totalFeedback}
-              positivePercentage={positivePercentage}
+              total={this.TotalFeedback()}
+              positivePercentage={this.PositiveFeedbackPercentage()}
             />
           ) : (
             <Notification message="There is no feedback" />
